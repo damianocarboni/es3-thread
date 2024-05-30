12bb22f8a0966c8c81d437ec16659298ca1327d8
lai2-24 <lai2-24@itismeucci.com>
2024-05-30 14:36:19 +0200

    first commit

--- es3/src/main/java/es_thread/Main.java
+++ es3/src/main/java/es_thread/Main.java
@@ -1,12 +1,20 @@
 package es_thread;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int array [] =  new int[10];
-
+        Miothread[] threads = new Miothread[10];
         for(int i = 0; i < 10; i++){
             Miothread t = new Miothread(array, i);
-            t.run();
+            threads[i] = t;
+            t.start();
         }
+        for(int i = 0; i < 10; i++){
+            threads[i].join();
+        }
+        for(int i = 0; i < 10; i++){
+            System.out.print(array [i] + " ");
+        }
+        System.out.println();
     }
 }
